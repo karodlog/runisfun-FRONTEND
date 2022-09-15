102467c6b1875a6826ac85f21d1829f822508f46
jean-luc VAN DE VELDE <jeanlucvdv@gmail.com>
2022-09-15 07:39:00 +0200

    trois

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,6 +2,24 @@ import React from "react";
 import pictoMan from "./person-solid.svg";
 
 const Card = ({ element }) => {
+
+  let changeColorBoxPrice;
+  let changeColorPromo;
+  if(element.promo==='new'){
+    changeColorBoxPrice= "containerPrice"
+    changeColorPromo= "promoShoes"
+  }
+  if(element.promo==='discount'){
+    changeColorBoxPrice="containerPriceOrange"
+    changeColorPromo= "promoShoesOrange"
+
+  }
+  if(element.promo==='good deal'){
+    changeColorBoxPrice="containerPriceRouge"
+    changeColorPromo= "promoShoesRouge"
+
+  }
+
   return (
     <div>
       <li className="containerCard">
@@ -19,12 +37,12 @@ const Card = ({ element }) => {
           <div className="infoShoes">
             <h4 className="activityShoes">{element.activity}</h4>
             <div className="infoPrice">
-              <h4 className="promoShoes">{element.promo}</h4>
+              <h4 className={changeColorPromo}>{element.promo}</h4>
               <h4 className="barredPriceShoes"><strike>{element.barredPrice}</strike></h4>
             </div>
           </div>
         </div>
-        <div className="containerPrice">
+        <div className={changeColorBoxPrice}>
           <h2 className="price">{element.price}</h2>
         </div>
       </li>
