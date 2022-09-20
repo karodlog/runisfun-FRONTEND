fcc96db40a63b3cf1b9941a1440810748ea992e5
jean-luc VAN DE VELDE <jeanlucvdv@gmail.com>
2022-09-20 10:21:19 +0200

    smallNav + recherche shoesWoman

--- src/pages/shoesManProduct/ShoesManProduct.jsx
+++ src/pages/shoesManProduct/ShoesManProduct.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import SmallNav from "../../components/smallNav/SmallNav";
 
 const ShoesManProduct = () => {
   const { id } = useParams();
@@ -16,70 +17,77 @@ const ShoesManProduct = () => {
 
   return (
     <div className="shoesManProduct">
-      <div className="cardShoesManProduct">
-        <div className="generalProduct">
-          <div className="containerImg">
-            <div className="container-img-showcase">
-              <img className="img-showcase" src={value.picture} alt="produit" />
+      <div className="bigContainer">
+        <SmallNav name={value.name} txt='Chaussures hommes'/>
+        <div className="cardShoesManProduct">
+          <div className="generalProduct">
+            <div className="containerImg">
+              <div className="container-img-showcase">
+                <img
+                  className="img-showcase"
+                  src={value.picture}
+                  alt="produit"
+                />
+              </div>
+              <div className="container_SmallPictures">
+                <div className="container_SmallPicture">
+                  <img
+                    src={value.picture}
+                    alt="detailProduct"
+                    className="smallPicture"
+                  />
+                </div>
+                <div className="container_SmallPicture">
+                  <img
+                    src={value.picture}
+                    alt="detailProduct"
+                    className="smallPicture"
+                  />
+                </div>
+                <div className="container_SmallPicture">
+                  <img
+                    src={value.picture}
+                    alt="detailProduct"
+                    className="smallPicture"
+                  />
+                </div>
+              </div>
             </div>
-            <div className="container_SmallPictures">
-              <div className="container_SmallPicture">
-                <img
-                  src={value.picture}
-                  alt="detailProduct"
-                  className="smallPicture"
-                />
-              </div>
-              <div className="container_SmallPicture">
-                <img
-                  src={value.picture}
-                  alt="detailProduct"
-                  className="smallPicture"
-                />
-              </div>
-              <div className="container_SmallPicture">
-                <img
-                  src={value.picture}
-                  alt="detailProduct"
-                  className="smallPicture"
-                />
-              </div>
+            <div className="product-infos">
+              <h4 className="brand">{value.brand}</h4>
+              <h2 className="nameProduct">{value.name}</h2>
+              <p>Prix: {value.price}</p>
+              <p className="descriptionProduct">{value.description}</p>
+              <form>
+                <div className="containerQuantity containersize">
+                  <label htmlFor="size">Pointure :</label>
+                  <select name="pointure" id="size">
+                    <option value="">Votre pointure ?</option>
+                    <option value="37">37</option>
+                    <option value="38">38</option>
+                    <option value="39">39</option>
+                    <option value="40">40</option>
+                    <option value="41">41</option>
+                    <option value="42">42</option>
+                    <option value="43">43</option>
+                    <option value="44">44</option>
+                    <option value="45">45</option>
+                    <option value="46">46</option>
+                    <option value="47">47</option>
+                  </select>
+                </div>
+                <div className="containerQuantity">
+                  <label htmlFor="quantity">Quantité :</label>
+                  <input type="number" id="quantity" value="1" onChange="" />
+                </div>
+                <button className="addPanier">Ajouter au panier</button>
+                <span className="adding-infos"></span>
+              </form>
             </div>
           </div>
-          <div className="product-infos">
-            <h4 className="brand">{value.brand}</h4>
-            <h2 className="nameProduct">{value.name}</h2>
-            <p>Prix: {value.price}</p>
-            <p className="descriptionProduct">{value.description}</p>
-            <form>
-              <div className="containerQuantity containersize">
-                <label htmlFor="size">Pointure :</label>
-                <select name="pointure" id="size">
-                  <option value="">Votre pointure ?</option>
-                  <option value="37">37</option>
-                  <option value="38">38</option>
-                  <option value="39">39</option>
-                  <option value="40">40</option>
-                  <option value="41">41</option>
-                  <option value="42">42</option>
-                  <option value="43">43</option>
-                  <option value="44">44</option>
-                  <option value="45">45</option>
-                  <option value="46">46</option>
-                  <option value="47">47</option>
-                </select>
-              </div>
-              <div className="containerQuantity">
-                <label htmlFor="quantity">Quantité :</label>
-                <input type="number" id="quantity" value="1" onChange="" />
-              </div>
-              <button className="addPanier">Ajouter au panier</button>
-              <span className="adding-infos"></span>
-            </form>
+          <div className="bandeauBas">
+            <p></p>
           </div>
-        </div>
-        <div className="bandeauBas">
-          <p></p>
         </div>
       </div>
     </div>
--- src/pages/shoesWoman/ShoesWoman.jsx
+++ src/pages/shoesWoman/ShoesWoman.jsx
@@ -7,6 +7,11 @@ import runnerWoman from "./bluewater-sweden-adIdmBMErV8-unsplash.png";
 
 const ShoesWoman = () => {
   const [datawoman, setdatawoman] = useState([]);
+  const [activity, setactivity] = useState("");
+  const [promo, setpromo] = useState("");
+
+  const categories = ["running", "trail", "hike"];
+  const promotions = ["new", "discount", "good deal"];
 
   const url = "http://localhost:5000/api/womanshoes/";
 
@@ -14,8 +19,48 @@ const ShoesWoman = () => {
     axios.get(url).then((res) => setdatawoman(res.data));
   }, []);
 
+  const resetRecherche = () => {
+    setactivity("");
+    setpromo("");
+  };
+
   return (
-    <div>
+    <div className="containerShoeswoman">
+      <div className="recherches">
+        <div className="radio-container">
+          <h4 className="activity">Activity</h4>
+          {categories.map((category) => (
+            <div className="selection">
+              <label htmlFor={category}>{category}</label>
+              <input
+                type="radio"
+                id={category}
+                name="categoryAct"
+                checked={category === activity}
+                onChange={(e) => setactivity(e.target.id)}
+              />
+            </div>
+          ))}
+          <h4 className="activity">Promo</h4>
+          {promotions.map((prom) => (
+            <div className="selection">
+              <label htmlFor={prom}>{prom}</label>
+              <input
+                type="radio"
+                id={prom}
+                name="categoryrProm"
+                checked={prom === promo}
+                onChange={(e) => setpromo(e.target.id)}
+                defaultChecked=""
+              />
+            </div>
+          ))}
+          <button className="reset" onClick={resetRecherche}>
+            RESET SEARCH
+          </button>
+        </div>
+      </div>
+
       <div className="runneuse">
         <img className="runnerWom" src={runnerWoman} alt="" />
       </div>
@@ -23,11 +68,14 @@ const ShoesWoman = () => {
       <ul className="listCard">
         <BarreFiltreShoes />
         {datawoman
-            .map((element, index)=>(
-                <>
-                <CardWoman key={index} element={element} />
-                </>
-            ))}
+          .filter((element) => element.activity.includes(activity))
+          .filter((element) => element.promo.includes(promo))
+
+          .map((element, index) => (
+            <>
+              <CardWoman key={index} element={element} />
+            </>
+          ))}
       </ul>
     </div>
   );
